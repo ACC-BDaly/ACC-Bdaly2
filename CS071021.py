099441f904497f254618fb80cbbb40eff93f6930
ACC-BDaly <92166848+ACC-BDaly@users.noreply.github.com>
2021-10-08 15:13:03 +0100

    Add files via upload

---
+++ CS071021.py
@@ -0,0 +1,14 @@
+#firstName=input('What is your first name? : ')
+#surName=input('What is your surname? : ')
+#print('Welcome', firstName, surName,)
+#ageNumber=int(input('What is your age? : '))
+#print('You will be', ageNumber+1, 'next year')
+#userHeight=float(input('What height are you? (Metres) : '))
+#print('User Name : ', firstName, surName)
+#print('Other Info :', 'Age -', ageNumber+1,)
+#print(userHeight, 'metres tall')
+
+#4
+firstNumber = int(input('Enter Number 1 :'))
+secondNumber = int(input('Enter Number 2 :'))
+print('Number 1 plus Number 2 equals', firstNumber + secondNumber)
