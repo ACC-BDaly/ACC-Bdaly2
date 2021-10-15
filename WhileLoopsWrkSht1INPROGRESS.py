135e8125742a7cc457f08fa0d681e01c75d142a5
ACC-BDaly <92166848+ACC-BDaly@users.noreply.github.com>
2021-10-15 15:09:48 +0100

    Add files via upload

---
+++ WhileLoopsWrkSht1INPROGRESS.py
@@ -0,0 +1,22 @@
+'''
+print('Please enter 10 numbers : ')
+totalNumber = 0
+counter = 0
+while counter < 10:
+    totalNumber = totalNumber + float(input('Enter Number :'))
+    counter = counter + 1
+endNumber = totalNumber / 10
+print(endNumber)
+
+
+totalNumber = 24
+counter = 0
+while counter <= 12:
+    counter += 1
+    print(counter * totalNumber)
+
+n = 1
+while n > 0:
+    n -= 0
+    print(n)
+'''
